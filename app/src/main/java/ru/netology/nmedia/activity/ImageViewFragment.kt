cc57251fc0534f61f1e38c0115b8d2f3afcb0e01
Digma <opt.artem@bk.ru>
2024-12-24 18:24:15 +0300

    one commit

---
+++ app/src/main/java/ru/netology/nmedia/activity/ImageViewFragment.kt
@@ -0,0 +1,54 @@
+package ru.netology.nmedia.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import ru.netology.nmedia.databinding.FragmentImageViewBinding
+
+
+class ImageViewFragment : Fragment() {
+
+    private var _binding: FragmentImageViewBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentImageViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val imageUrl = arguments?.getString(ARG_IMAGE_URL) ?: return
+
+        Glide.with(this)
+            .load(imageUrl)
+            .into(binding.imageView)
+
+        requireActivity().window.decorView.setBackgroundColor(resources.getColor(android.R.color.black))
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val ARG_IMAGE_URL = "image_url"
+
+        fun newInstance(imageUrl: String): ImageViewFragment {
+            val fragment = ImageViewFragment()
+            val args = Bundle()
+            args.putString(ARG_IMAGE_URL, imageUrl)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
